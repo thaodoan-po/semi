ba777e2d84302a7b56c37f98d0809268cfadab8c
doanthao1503 <48922098+doanthao1503@users.noreply.github.com>
2020-06-25 12:14:10 +0700

    Update getPort.js

--- getPort.js
+++ getPort.js
@@ -4,12 +4,22 @@ module.exports = function getPort(ref){
     exec('sudo lsof -i -P -n | grep LISTEN', (err, stdout, stderr) => {
         console.log(stderr);
         //reg to get port
-        var reg = /(\w+)d.\w?\s+\d+\s+\w+.?\w+\s+\w+\s+IPv4\s+\d+\s+\w+\s+(TCP|UDP)\s+(\d+.\d+.\d+.\d+):(\d+)/gim;
+        var reg = /(\w+d).\w?\s+\d+\s+\w+.?\w+\s+\w+\s+(IPv4|IPv6)\s+\d+\s+\w+\s+(TCP|UDP)\s+\d+.\d+.\d+.\d+:(\d+)/gim;
         //get port and service
         while(tempPort = reg.exec(stdout)) {
             //set data to firebase realtime database
             ref.child(tempPort[4]).set({
-                protocol: tempPort[2],
+                protocol: tempPort[3],
+                service: tempPort[1]
+            });
+        }
+        //reg to get port
+        var reg_ = /^(\w+)\s+\w+\s+\w+\s+\w+\s+(IPv4|IPv6)\s+\w+\s+\w+\s+(TCP|UDP)\s+\*:(\d+)/gim;
+        //get port and service
+        while(tempPort = reg.exec(stdout)) {
+            //set data to firebase realtime database
+            ref.child(tempPort[4]).set({
+                protocol: tempPort[3],
                 service: tempPort[1]
             });
         }
