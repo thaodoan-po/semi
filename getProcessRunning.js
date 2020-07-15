bb82f5fdc857a43c39e500de43f30ef6c28bde3f
doanthao1503 <48922098+doanthao1503@users.noreply.github.com>
2020-07-15 11:07:40 +0700

    Add files via upload

--- getProcessRunning.js
+++ getProcessRunning.js
@@ -1,15 +1,16 @@
 var { exec } = require('child_process');
 module.exports = function getProcessRunning(ref) {
     //execute cmd to get information
-    exec('ps -eo pid,comm,%mem,%cpu --sort=-%mem', (err, stdout, stderr) => {
-        var reg = /^\s+(\d+)\s(\w+-\w+|\w+)\s+(\d+.{2})\s+(\d+.{2})$/gim;
+    exec('ps -eo pid,comm,cmd,%mem,%cpu --sort=-%mem', (err, stdout, stderr) => {
+        var reg = /^\s+(\d+)\s(\w+-\w+|\w+)\s+(\/.*?)\s+(\d+.{2})\s+(\d+.{2})$/gim;
         var temp;
         var process = {};
         while (temp = reg.exec(stdout)) {
             process[temp[1]] = {
                 cmd: temp[2],
-                mem: temp[3],
-                cpu: temp[4]
+                dir: temp[3],
+                mem: temp[4],
+                cpu: temp[5]
             }
         }
         ref.set(process)
